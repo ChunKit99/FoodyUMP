7858e473c867cda60247fa7b8747e21e3b66b9b4
Yong Liang T <yongliang99@gmail.com>
2022-05-20 15:21:01 +0800

    update all and done rider home page

--- assets/js/rider_qr.js
+++ assets/js/rider_qr.js
@@ -1,3 +1,53 @@
+//rider_home.html
+function record_edit(){
+  if (confirm('Are you sure to edit?')==true) {
+      alert('Saved!');
+    } else {
+      // Do nothing!
+      alert('No item edited.');
+    }
+  }
+  
+//rider_home.html
+  function record_delete(){
+      if (confirm('Are you sure to delete?')) {
+          var res_profile = document.getElementsByClassName('record1');
+          for(var i=0; i< xxx.length; i++){
+                box = xxx[i];
+                t=text[i];
+            if(box.checked==true) {
+                switch(i){
+                    case 0:
+                        i = 0;
+                        document.getElementById('td').value='';
+                        box.checked=false;
+                        break;
+                    case 1:
+                        i = 1;
+                        document.getElementById('td').value='';
+                        box.checked=false;
+                        break;
+                    case 2:
+                          i = 2;
+                          document.getElementById('td').value='';
+                          box.checked=false;
+                          break;
+                    case 3:
+                          i = 3;
+                          document.getElementById('td').value='';
+                          box.checked=false;
+                          break;
+                  }
+              }
+      
+          }
+             alert('Deleted!');
+        } else {
+          alert('No item deleted.');
+        }
+  }
+
+//rider_order.html
 function qr(img){
     // Get the modal
     var modal = document.getElementById("myModal");
@@ -26,6 +76,7 @@ function qr(img){
     }
 }
 
+//rider_report.html
 function graph(myChart){
   var xValues = [50,60,70,80,90,100,110,120,130,140,150];
                 var yValues = [7,8,8,9,9,9,10,11,14,14,15];
@@ -49,4 +100,44 @@ function graph(myChart){
                     }
                 }
                 });
+}
+
+//rider_delivery_notes_details.html
+function orderUpdate(){
+  if (confirm("Are you sure to update?")==true) {
+    alert('Updated!');
+    window.location.href = "rider_order.html";
+  } else {
+    alert('No changed done.');
+  }
+}
+
+//rider_delivery_notes_details.html
+function orderDelete(){
+  if (confirm("Are you sure to delete?")==true) {
+    alert('Deleted!');
+    window.location.href = "rider_order.html";
+  } else {
+    alert('No changed done.');
+  }
+}
+
+//rider_delivery_records_details.html
+function recordsUpdate(){
+  if (confirm("Are you sure to update?")==true) {
+    alert('Updated!');
+    window.location.href = "rider_home.html";
+  } else {
+    alert('No changed done.');
+  }
+}
+
+//rider_delivery_records_details.html
+function recordsDelete(){
+  if (confirm("Are you sure to delete?")==true) {
+    alert('Deleted!');
+    window.location.href = "rider_home.html";
+  } else {
+    alert('No changed done.');
+  }
 }
