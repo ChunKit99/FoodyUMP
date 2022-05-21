67b28920bfd06dd8f2e347c0369930969de4ae6e
LIEW CHUN KIT <61322992+ChunKit99@users.noreply.github.com>
2022-05-22 01:01:59 +0800

    fix missing rider_id in orderlist

--- general_user/views/complaint_insert.php
+++ general_user/views/complaint_insert.php
@@ -8,7 +8,19 @@ include_once($path);
 
 extract($_POST);
 //$query = "INSERT INTO book (nama,email,tarikh,masa,komen) VALUES('$nama','$email','$tarikh','$masa','$komen')";
-$query = "INSERT INTO complaint(order_id, `user_id`, complaint_date, complaint_time, complaint_type, complaint_desc, complaint_status) VALUES ('$chooseOrderID','$staticUserID','$staticDate','$staticTime','$chooseType','$descriptionComplaint','In Investigation')";
+
+$query_search_rider = "SELECT * FROM orderlist WHERE `order_id` = $chooseOrderID";
+if (mysqli_query($conn, $query_search_rider)) {
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($conn);
+}
+
+$result = mysqli_query($conn, $query_search_rider);
+$row1 = mysqli_fetch_assoc($result);
+$riderid = $row1["rider_id"];
+
+
+$query = "INSERT INTO complaint(order_id, `user_id`, rider_id, complaint_date, complaint_time, complaint_type, complaint_desc, complaint_status) VALUES ('$chooseOrderID','$staticUserID','$riderid','$staticDate','$staticTime','$chooseType','$descriptionComplaint','In Investigation')";
 
 if (mysqli_query($conn, $query)) {
 
